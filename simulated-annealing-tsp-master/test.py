d6ac0127acc5fd5a1e45f7ce35bd4018a55b7067
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-09-26 14:58:54 -0500

    Enfriamiento simulado

---
+++ simulated-annealing-tsp-master/test.py
@@ -0,0 +1,36 @@
+import os
+from anneal import SimAnneal
+import matplotlib.pyplot as plt
+import random
+import time
+
+def read_coords(path):
+    coords = []
+    with open(path, "r") as f:
+        for line in f.readlines():
+            line = [float(x.replace("\n", "")) for x in line.split(" ")]
+            coords.append(line)
+    return coords
+
+
+def generate_random_coords(num_nodes):
+    return [[random.uniform(-1000, 1000), random.uniform(-1000, 1000)] for i in range(num_nodes)]
+
+
+if __name__ == "__main__":
+    
+    path = r'/data/datos.txt'
+    separador = os.path.sep
+    dir_actual = os.path.dirname(os.path.abspath(__file__))
+    dir = separador.join(dir_actual.split(separador)[:-1])
+    newPath = dir + path
+    
+    coords = read_coords(newPath)  
+    sa = SimAnneal(coords, stopping_iter=900)
+    startTime = time.time()
+    sa.anneal()
+    sa.visualize_routes()
+    sa.plot_learning()
+    endTime = time.time()
+    
+    print(f"Tiempo de ejecución: {(endTime - startTime):.6f} segundos")
